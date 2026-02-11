df07838039c59f65760c3e1d532b31bad913fda0
Elif Altındaş <altindaselif@gmail.com>
2026-02-11 17:29:24 +0300

    Code review fixes

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ This is a News Homepage that you can use on your websites.
 
 ## Links
 
-- **GitHub Repository:** [View Code](https://github.com/altindaselif/newsletter-sign-up-with-success-message-main)
-- **Live Demo:** [View Live Site](https://altindaselif.github.io/newsletter-sign-up-with-success-message-main/)
+- **GitHub Repository:** [View Code](https://github.com/altindaselif/18-news-homepage-main)
+- **Live Demo:** [View Live Site](https://altindaselif.github.io/18-news-homepage-main/)
 
 ## Built With
 
--- script.js
+++ script.js
@@ -7,12 +7,14 @@ const closeNavbarMenu = function () {
   navbarMenu.classList.remove("active");
   overlay.classList.remove("active");
   document.body.classList.remove("scroll-lock");
+  menuOpenButton.setAttribute("aria-expanded", "false");
 };
 
 menuOpenButton.addEventListener("click", function () {
   navbarMenu.classList.add("active");
   overlay.classList.add("active");
   document.body.classList.add("scroll-lock");
+  menuOpenButton.setAttribute("aria-expanded", "true");
 });
 
 menuCloseButton.addEventListener("click", closeNavbarMenu);
